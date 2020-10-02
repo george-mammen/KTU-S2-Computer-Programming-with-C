bf7f3b3e87df7c6391f68e85003e9956379b1560
Devika-H1 <72244373+Devika-H1@users.noreply.github.com>
2020-10-02 11:25:26 +0530

    Update Array - Sorting.c

--- Array - Sorting.c
+++ Array - Sorting.c
@@ -4,34 +4,34 @@ be modified. From the main() print both the arrays.*/
 
 #include<stdio.h>
 
-int sortarr(int arr[10])
+int sortarr(int A[10])
 {
-    int n=10,temp;
+    int n=10, temp;
     int i,j;  
-    for(i=0;i<n-1;i++) 
+    for( i=0; i<n-1; i++) 
     {  
-        for(j=0;j<n-i-1;j++)  
-            if(arr[j]>arr[j+1])
+        for( j=0; j<n-i-1; j++)  
+            if(A[j] > A[j+1])
             {  
-                temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
+                temp = A[j];
+                A[j] = A[j+1];
+                A[j+1] = temp;
             }
     }
     printf("\n Sorted Array = ");
-    for(i=0;i<10;i++)
+    for( i=0; i<10; i++)
     {
-        printf("%d,",arr[i]);
+        printf("%d,",A[i]);
     }
 }
 
 int main(){
-    int arr[10]={13,3,45,43,78,23,65,29,30,2},i=0;
+    int A[10]={13,3,45,43,78,23,65,29,30,2}, i=0;
         
-    printf("\n Unsorted Array= ");
-    for(i;i<10;i++)
+    printf("\n Unsorted Array = ");
+    for( i; i<10; i++)
     {
-        printf("%d, ",arr[i]);
+        printf("%d, ",A[i]);
     }
-    sortarr(arr);
+    sortA(A);
 }
